2d48c2a9fdc589b6ca2351538d11565997e34153
Samuel <samraca81@gmail.com>
2021-10-22 18:09:22 -0500

    Jump added

--- Player.cs
+++ Player.cs
@@ -3,26 +3,93 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace BackgroundTest
 {
     class Player : MovingSprite
     {
-        
-        public Player( Rectangle newRectangle, int PlayerHealth, Game1 newRoot, String textureName)
+        Vector2 VectorialPosition;
+        Vector2 Gravity;
+        Point CardinalPosition;
+        bool hasjumped;
+        public Player(Rectangle newRectangle, int PlayerHealth, Game1 newRoot, String textureName)
         {
-            
+            VectorialPosition.X = newRectangle.X;
+            VectorialPosition.Y = newRectangle.Y;
+            hasjumped = true;
             rectangle = newRectangle;
             HealthPoints = PlayerHealth;
             Initialize();
-            LoadContent(newRoot,textureName);
+            LoadContent(newRoot, textureName);
         }
 
         public void Update()
         {
             FramesPerSecond();
+            Movement();
         }
 
-        
+        public void Movement()
+        {
+            VectorialPosition += Gravity;
+            if (Keyboard.GetState().IsKeyDown(Keys.Right))
+            {
+                VectorialPosition.X++;
+                Gravity.X = 3f;
+            }
+            else if (Keyboard.GetState().IsKeyDown(Keys.Left))
+            {
+                VectorialPosition.X--;
+                Gravity.X = -3f;
+            }
+            else
+            {
+                Gravity.X = 0f;
+            }
+            if (Keyboard.GetState().IsKeyDown(Keys.Space) && hasjumped == false)
+            {
+                VectorialPosition.Y -= 19f;
+                Gravity.Y = -20f;
+                hasjumped = true;
+            }
+
+            if (hasjumped)
+            {
+                float i = 1;
+                Gravity.Y += 0.5f * i;
+            }
+
+
+            if (VectorialPosition.Y + rectangle.Height >= 700)
+            {
+                hasjumped = false;
+            }
+
+            if (hasjumped == false)
+            {
+                Gravity.Y = 0f;
+            }
+
+            if (VectorialPosition.X <= -20)
+            {
+                VectorialPosition.X = -20f;
+            }
+            if (VectorialPosition.X <= 0)
+            {
+                VectorialPosition.X = 0;
+            }
+            CardinalPosition = Vector2ToPoint(VectorialPosition, CardinalPosition);
+            rectangle.X = CardinalPosition.X;
+            rectangle.Y = CardinalPosition.Y;
+
+        }
+
+        public Point Vector2ToPoint(Vector2 newVector, Point newPoint)
+        {
+            newPoint.X = Convert.ToInt32(newVector.X);
+            newPoint.Y = Convert.ToInt32(newVector.Y);
+            return newPoint;
+        }
     }
 }
